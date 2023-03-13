0e7d82f465501a5b81b563553d36546d357b2664
wandeadeyeye <54033933+wandeadeyeye@users.noreply.github.com>
2023-03-13 21:39:12 +0100

    basic_operator

--- js-starter/basic_operators.js
+++ js-starter/basic_operators.js
@@ -5,4 +5,24 @@ var totalDistance, johnCovered, samCovered;
 totalDistance = 50;
 
 johnCovered = totalDistance - 15;
-console.log(johnCovered)
+console.log(johnCovered);
+
+samCovered = totalDistance - 23;
+console.log(samCovered);
+
+console.log(samCovered * 2);
+
+console.log(samCovered / 2);
+
+// Logical Operators
+
+var distanceToSchool, distanceToHome;
+
+distanceToHome = 15;
+distanceToSchool = 8;
+
+var isCloser = distanceToHome < distanceToSchool;
+console.log(isCloser);
+
+var isCloser = distanceToHome > distanceToSchool;
+console.log(isCloser);
