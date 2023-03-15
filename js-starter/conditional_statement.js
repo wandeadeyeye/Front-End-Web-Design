cb438ab06e43c4d249408b7de79b1076b40d070f
wandeadeyeye <54033933+wandeadeyeye@users.noreply.github.com>
2023-03-15 17:42:49 +0100

    added-files

---
+++ js-starter/conditional_statement.js
@@ -0,0 +1,27 @@
+// CONDITIONAL STATEMENT
+
+// var grades;
+
+// grades = 90;
+
+// if (grades < 40) {
+//     console.log('sorry, you will have to retake this class');
+// }else if (grades >= 40 && grades < 45) {
+//     console.log('you have been promoted to the next round, you will have to retake only the test ');
+// }else{
+//     console.log('you have been promoted to the next round')
+// }
+
+// QUIZZ
+
+var age;
+
+age = 19;
+
+if (age < 18) {
+    console.log('sorry buddy, too young no entry');
+}else if (age >= 18 && age < 23) {
+    console.log('you can come in but no alcohol alright');
+}else{
+    console.log('come in buddy lets get drunk');
+}
