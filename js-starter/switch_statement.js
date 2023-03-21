107c44b783799b66128a98c16d9780d003961a76
wandeadeyeye <54033933+wandeadeyeye@users.noreply.github.com>
2023-03-21 18:08:11 +0100

    switch_statement

--- js-starter/switch_statement.js
+++ js-starter/switch_statement.js
@@ -26,20 +26,24 @@
 
 var age;
 
-age = 19;
+age = 24;
 
 switch (true) {
-    if case (age < 18):
-    console.log('sorry buddy, too young no entry');
+    case (age < 18):
+        console.log('sorry buddy, too young no entry');
         break;
-
+    case(age >= 18 && age <= 23):
+        console.log('you can come in but no alcohol alright');
+        break;
     default:
+        console.log('come in buddy lets get drunk');
         break;
 }
-if (age < 18) {
-    console.log('sorry buddy, too young no entry');
-}else if (age >= 18 && age < 23) {
-    console.log('you can come in but no alcohol alright');
-}else{
-    console.log('come in buddy lets get drunk');
-}
+
+// if (age < 18) {
+//     console.log('sorry buddy, too young no entry');
+// }else if (age >= 18 && age < 23) {
+//     console.log('you can come in but no alcohol alright');
+// }else{
+//     console.log('come in buddy lets get drunk');
+// }
