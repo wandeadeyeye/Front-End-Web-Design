6d0a74dbc4187f6f45cd38d174fb76d56d44fb44
wandeadeyeye <54033933+wandeadeyeye@users.noreply.github.com>
2023-03-22 21:27:43 +0100

    quiz_2

---
+++ js-starter/quiz_2.js
@@ -0,0 +1,20 @@
+
+//QUIZ 1
+
+// var number = 132;
+
+// while (number <= 148) {
+//     console.log(number);
+//     number += 2;
+// }
+
+//QUIZ 2
+
+var number = 25;
+
+while (number <= 100) {
+    if (number % 7 === 0) {
+    console.log(number);
+    }
+    number++;
+}
