49e8d41d1815d36e626159937075c3363c7fe6f1
wandeadeyeye <54033933+wandeadeyeye@users.noreply.github.com>
2023-03-29 16:04:31 +0100

    Update function_expression.js

--- js-starter/function_expression.js
+++ js-starter/function_expression.js
@@ -1,1 +1,19 @@
 ////////////// FUNCTION DECLERATION AND EXPRESSION //////////////
+
+// function calculateAge(birthYear) {
+//     return 2023 - birthYear;
+// }
+
+// var age = calculateAge(2000);
+// console.log('john is', age, 'years old');
+
+var age = function (birthYear) {
+    return 2023 - birthYear;
+}
+
+var calculateAge = age(2000);
+console.log('He is', calculateAge, 'years old');
+
+calculateAge = 'john';
+console.log('His name is ',calculateAge);
+
