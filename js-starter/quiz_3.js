4307167550256120b0c1feeff92a678db4cfa934
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-06-11 20:55:28 +0100

    quizz-3

--- js-starter/quiz_3.js
+++ js-starter/quiz_3.js
@@ -1,11 +1,11 @@
 /* QUIZ 3. odd numbers between 132 and 148 */
 
-// for (var number = 132; number <= 148; number++) {
-//     if (number % 2 !== 0) {
-//     console.log(number);   
-//     }
+for (var number = 132; number <= 148; number++) {
+    if (number % 2 !== 0) {
+    console.log(number);   
+    }
     
-// }
+}
 
 /*  QUIZ 4. Numbers between 25 and 100, divisible by 5 and 7 */
 
