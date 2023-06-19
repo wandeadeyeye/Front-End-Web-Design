38ca2a33fdb964c19839d0f1e8d93b5776496734
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-06-19 21:24:06 +0100

    singleComment updated

--- react-app/src/SingleComment.js
+++ react-app/src/SingleComment.js
@@ -1,24 +1,26 @@
 import React from "react";
 import profile1 from './image/img1.jpg';
+import profile2 from './image/img2.jpg';
+import profile3 from './image/img3.jpg';
 
-const SingleComment = () => {
+const SingleComment = (props) => {
     return(
         <div className='ui comment'>
             <div className='comment'>
                 <a href="/" className='avatar'>
-                    <img src={profile1} alt="Profile Picture" width={60}/>
+                    <img src={props.picture} alt="Profile" width={60}/>
                 </a>
                 <div className='content'>
                     <a href="/" className='authur'>
-                        John
+                        {props.name}
                     </a>
                     <div className='metadata'>
                         <span className='data'>
-                            Today at 6:45PM
+                            {props.date}
                         </span>
                     </div>
                     <div className='text'>
-                        its amazing
+                        {props.text}
                     </div>
                 </div>
             </div>
