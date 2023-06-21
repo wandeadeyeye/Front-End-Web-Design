52560f4fb958d9f477d5ec60cb35ab4c8a41f1ae
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-06-21 14:12:50 +0100

    hemisphere file

---
+++ hemisphere/src/hemisphereDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const hemisphereDisplay = () => {
+    return(
+        <div>
+            Hey you are in Northern hemisphere
+        </div>
+    )
+}
+
+export default hemisphereDisplay;
