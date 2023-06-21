dc460cf249d5fe16b638d2a12868998667382094
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-06-21 14:11:58 +0100

    index updated

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const App = () => {
+
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (error) => console.log(error)
+//   );
+//     return(
+//         <div>
+//             Hello, world!
+//         </div>
+//     )
+// }
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.log(error)
+    );
+    return(
+      <div>
+        you are in the Northern hemisphere
+      </div>
+    )
+  };
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
