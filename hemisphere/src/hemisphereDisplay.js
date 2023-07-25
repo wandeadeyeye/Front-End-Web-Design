e727254361a34460d74ef6ddd72e821cca123911
Wande B <54033933+wandeadeyeye@users.noreply.github.com>
2023-07-25 20:41:25 +0100

    Update hemisphereDisplay.js

--- hemisphere/src/hemisphereDisplay.js
+++ hemisphere/src/hemisphereDisplay.js
@@ -1,16 +1,18 @@
 import React from "react";
-import southernHemisphere from './'
-import northernHemisphere from './'
+import northernHemisphere from './image/Northern.jpg';
+import southernHemisphere from './image/Southern.jpg';
 
 
 const HemisphereDisplay = ({latitude}) => {
+
     console.log(latitude);
-    const picture = latitude > 0 ? 'Northern Hemisphere' : 'Southern Hemisphere';
+
+    const picture = latitude > 0 ? northernHemisphere : southernHemisphere;
     const hemisphere = latitude > 0 ? 'Northern Hemisphere' : 'Southern Hemisphere';
 
     return(
         <div>
-            <img src { picture } alt=""/>
+            <img src={ picture } alt=""/>
            { hemisphere }
         </div>
     )
